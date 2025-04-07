3d2495ff6eddc9b1ab93ddd0392fa617383b9b2d
Äkwav <ekwav@coflnet.com>
2025-04-07 11:34:09 +0200

    allow specififying mapping fix #98

--- Services/GenericSettingsUpdater.cs
+++ Services/GenericSettingsUpdater.cs
@@ -1,25 +1,36 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Coflnet.Sky.Commands.Shared
 {
     public class GenericSettingsUpdater : SettingsUpdater
     {
-        public GenericSettingsUpdater() 
+        private Dictionary<string, Func<object, object>> mapper = new();
+        public GenericSettingsUpdater()
         {
             options.Clear();
         }
-        public void AddSettings(Type type, string prefix = "")
+        public void AddSettings(Type type, string prefix = "", Func<object, object> mapper = null)
         {
+            if (mapper != null)
+                this.mapper.Add(prefix, mapper);
             AddSettings(type.GetFields(), prefix);
         }
 
         public object Update(object target, string key, string value)
         {
-            if(!options.TryGetValue(key, out SettingDoc doc))
+            if (!options.TryGetValue(key, out SettingDoc doc))
             {
                 var closest = options.Keys.OrderBy(k => Fastenshtein.Levenshtein.Distance(k.ToLower(), key.ToLower())).First();
                 throw new UnknownSettingException(key, closest);
+            }
+            if (!string.IsNullOrEmpty(doc.Prefix))
+            {
+                if (mapper.TryGetValue(doc.Prefix, out var func))
+                {
+                    target = func(target);
+                }
             }
             return UpdateValueOnObject(value, doc.RealName, target);
         }
