c178a605448e1141d82bba51642dc75e3e4145a0
Äkwav <ekwav@coflnet.com>
2025-08-04 00:55:46 +0200

    Add composter service for #104

--- Services/DIHandler.cs
+++ Services/DIHandler.cs
@@ -179,6 +179,7 @@ namespace Coflnet.Sky.Commands.Shared
             services.AddSingleton<ISniperClient, SniperClient>();
             services.AddSingleton<EventBrokerClient>();
             services.AddSingleton<MinionService>();
+            services.AddSingleton<ComposterService>();
             services.AddSingleton<ISubscriptionsApi>(context =>
             {
                 var config = context.GetRequiredService<IConfiguration>();
