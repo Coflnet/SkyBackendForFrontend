0b96cec1002798a192fa0278fe2f17d90db57e72
Äkwav <ekwav@coflnet.com>
2025-09-03 12:15:23 +0200

    extend ingredient count to be long

--- Models/Crafts/Ingredient.cs
+++ Models/Crafts/Ingredient.cs
@@ -3,7 +3,7 @@ namespace Coflnet.Sky.Crafts.Models
     public class Ingredient
     {
         public string ItemId { get; set; }
-        public int Count { get; set; }
+        public long Count { get; set; }
         public double Cost { get; set; }
         public string Type { get; set; }
     }
