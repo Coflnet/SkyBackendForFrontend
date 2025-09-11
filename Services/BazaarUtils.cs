01db892539a3f2bdafa1aabfbad5e594d0e2ca29
Äkwav <ekwav@coflnet.com>
2025-09-11 10:06:01 +0200

    Add bazaar utils search value retrieval

---
+++ Services/BazaarUtils.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text.RegularExpressions;
+using Coflnet.Sky.Core;
+
+namespace Coflnet.Sky.Commands.Shared;
+
+public class BazaarUtils
+{
+    public static string GetSearchValue(string tag, string name)
+    {
+        if (tag.StartsWith("ENCHANTMENT_"))
+        {
+            // remove enchant from end
+            name = name.Substring(0, name.Length - 10);
+            var number = Regex.Match(tag, @"\d+").Value;
+            var converted = Roman.To(int.Parse(number));
+            name = $"{name.Trim()} {converted}";
+        }
+        if (tag.StartsWith("ENCHANTMENT_ULTIMATE"))
+        {
+            name = name.Replace("ultimate ", "", StringComparison.OrdinalIgnoreCase);
+        }
+        return name;
+    }
+}
