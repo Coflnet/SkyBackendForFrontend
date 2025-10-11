aada23697134a1bed10c9ad924a64a985ac80555
Äkwav <ekwav@coflnet.com>
2025-10-11 15:09:47 +0200

    make nocookie nullable

--- Models/Mod/DescriptionSetting.cs
+++ Models/Mod/DescriptionSetting.cs
@@ -56,7 +56,7 @@ public class DescriptionSetting
     [SettingsDoc("Replace lore color white with this color code")]
     public string ReplaceWhiteWith;
     [SettingsDoc("Enables no cookie workarounds, can be incompatible with other mods and considered a macro", "nocookie")]
-    public bool NoCookie;
+    public bool? NoCookie;
 
     public HighlightInfo HighlightInfo { get; set; }
 }
